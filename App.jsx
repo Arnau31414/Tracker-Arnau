0d8f9fb366e908e745fe5157381260ca88ba7a57
Arnau31414 <lobo63134@gmail.com>
2025-04-01 10:35:11 +0100

    Primer Commit

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '50px' }}>
+      <h1>Tracker Arnau</h1>
+      <p>La base de la teva app ja és viva! 🚀</p>
+    </div>
+  );
+}
